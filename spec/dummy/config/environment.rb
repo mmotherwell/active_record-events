12f50468b9d6b395e9341191e8a556d1b4af9218
Bartosz Pieńkowski <pienkowb@gmail.com>
2024-05-20 01:44:01 +0200

    Remove the default from `ENV.fetch`

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -2,14 +2,14 @@ require File.expand_path('boot', __dir__)
 
 require 'active_record'
 
-Bundler.require(:default, ENV.fetch('RAILS_ENV', nil))
+Bundler.require(:default, ENV.fetch('RAILS_ENV'))
 
 # Load application files
 Dir["#{__dir__}/../app/**/*.rb"].sort.each { |f| require f }
 
 # Load the database configuration
 config_file = File.expand_path('database.yml', __dir__)
-config = YAML.load_file(config_file)[ENV.fetch('RAILS_ENV', nil)]
+config = YAML.load_file(config_file)[ENV.fetch('RAILS_ENV')]
 
 ActiveRecord::Base.logger = Logger.new(File::NULL)
 
